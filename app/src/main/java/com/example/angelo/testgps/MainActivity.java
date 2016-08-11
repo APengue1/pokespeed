a571c7473add58ffdf9420ad0e15217d679df817
Angelo Pengue <apengue1@gmail.com>
2016-08-10 21:21:38 -0400

    speed service base skeleton

--- app/src/main/java/com/example/angelo/testgps/MainActivity.java
+++ app/src/main/java/com/example/angelo/testgps/MainActivity.java
@@ -18,123 +18,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements LocationListener {
+public class MainActivity extends Activity {
 
-    private LocationManager locationManager;
-    private NotificationManager notificationManager;
+
     private TextView kmh;
-    private Location lastLocation;
-    private Long lastTime;
-    private NotificationCompat.Builder mBuilder;
-    private static final int NOTIFY_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         kmh = (TextView)findViewById(R.id.kmh);
-        lastLocation = null;
-        lastTime = null;
-        mBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.poke_speed)
-                .setContentTitle("Pokespeed")
-                .setOngoing(true);
-
-        Intent resultIntent = new Intent(this, MainActivity.class);
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                resultIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-        );
-        mBuilder.setContentIntent(resultPendingIntent);
-
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         getPermissions();
-        requestLocation();
-    }
 
-    private void requestLocation() {
-        try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    500l,
-                    0.5f,
-                    this);
-        }
-        catch(SecurityException e) {
-            kmh.setText(e.getMessage());
-        }
-    }
-
-    private void removeLocation() {
-        try {
-            locationManager.removeUpdates(this);
-        }
-        catch(SecurityException e) {
-            kmh.setText(e.getMessage());
-        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        requestLocation();
-        notificationManager.notify(this.NOTIFY_ID, mBuilder.build());
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        removeLocation();
-        notificationManager.cancelAll();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if(location.hasSpeed()) {
-            Float speed = location.getSpeed(); // m/s
-            speed = speed * 60 * 60 / 1000; // km/h
-            //kmh.setText(speed.toString());
-            setKm(speed);
-        }
-        else if(this.lastLocation == null || this.lastTime == null) {
-            this.lastLocation = location;
-            this.lastTime = location.getTime(); // ms
-        }
-        else {
-            float distanceCovered = location.distanceTo(this.lastLocation); // m
-            long timeElapsed = location.getTime() - this.lastTime; //ms
-            this.lastLocation = location;
-            this.lastTime = location.getTime();
-            Float speed = distanceCovered / timeElapsed; // m/ms
-            speed = speed * 60 * 60 * 60 / 1000; // km/h
-            //kmh.setText(speed.toString());
-            setKm(speed);
-        }
     }
 
     private void setKm(Float speed) {
         String speedStr = speed.toString();
         kmh.setText(speedStr);
-        mBuilder.setContentText(speedStr);
-        notificationManager.notify(this.NOTIFY_ID, mBuilder.build());
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
     }
 
     private void getPermissions() {
---
+++ app/src/main/java/com/example/angelo/testgps/SpeedService.java
@@ -0,0 +1,113 @@
+package com.example.angelo.testgps;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class SpeedService extends Service implements LocationListener {
+
+    private LocationManager locationManager;
+    private Location lastLocation;
+    private Long lastTime;
+    private static final int NOTIFY_ID = 1;
+
+    public SpeedService() {
+    }
+
+    @Override
+    public void onCreate() {
+        lastLocation = null;
+        lastTime = null;
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return START_NOT_STICKY;
+        //return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void requestLocation() {
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    500l,
+                    0.5f,
+                    this);
+        }
+        catch(SecurityException e) {
+            //kmh.setText(e.getMessage());
+        }
+    }
+
+    private void removeLocation() {
+        try {
+            locationManager.removeUpdates(this);
+        }
+        catch(SecurityException e) {
+           // kmh.setText(e.getMessage());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location.hasSpeed()) {
+            Float speed = location.getSpeed(); // m/s
+            speed = speed * 60 * 60 / 1000; // km/h
+            //kmh.setText(speed.toString());
+            setKm(speed);
+        }
+        else if(this.lastLocation == null || this.lastTime == null) {
+            this.lastLocation = location;
+            this.lastTime = location.getTime(); // ms
+        }
+        else {
+            float distanceCovered = location.distanceTo(this.lastLocation); // m
+            long timeElapsed = location.getTime() - this.lastTime; //ms
+            this.lastLocation = location;
+            this.lastTime = location.getTime();
+            Float speed = distanceCovered / timeElapsed; // m/ms
+            speed = speed * 60 * 60 * 60 / 1000; // km/h
+            //kmh.setText(speed.toString());
+            setKm(speed);
+        }
+    }
+
+    private void setKm(Float speed) {
+        String speedStr = speed.toString();
+//        kmh.setText(speedStr);
+//        mBuilder.setContentText(speedStr);
+//        notificationManager.notify(this.NOTIFY_ID, mBuilder.build());
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
