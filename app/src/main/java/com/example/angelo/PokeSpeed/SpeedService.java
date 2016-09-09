50b93b9f96878b87e235ab7994d58221fda6c545
Angelo Pengue <apengue1@gmail.com>
2016-09-09 17:35:52 -0400

    Looser GPS accuracy restraints

--- app/src/main/java/com/example/angelo/PokeSpeed/SpeedService.java
+++ app/src/main/java/com/example/angelo/PokeSpeed/SpeedService.java
@@ -43,7 +43,7 @@ public class SpeedService extends Service implements LocationListener{
 
     private static final String LOCATION_WAIT = "0.00";
     private static final String TURN_ON_GPS = "Turn on gps";
-    private static final int MIN_ACCURACY = 50;
+    private static final int MIN_ACCURACY = 100;
     private static final long MIN_TIME_FAST = 0;
     private static final long MIN_TIME_DEFAULT = 1500;
 
